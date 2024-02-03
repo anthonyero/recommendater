5b59f8ca554f3033110c518ef78cdf3185b7fd70
Pedro Gonzalez <orion@Pedros-MacBook-Pro.local>
2024-02-03 11:19:00 -0600

    adding the javascript to the script.js code to render a map.

--- assets/script.js
+++ assets/script.js
@@ -16,4 +16,51 @@ google.maps.event.addListener(map, 'click', function(event) {
     lat: latitude,
     lng: longitude
   };
-})
+  fetch("https://google-maps-geocoding.p.rapidapi.com/geocode/json?language=en&latlng=" + latitude + "," + longitude, {
+    method: "GET",
+    headers: {
+      "x-rapidapi-host": "google-maps-geocoding.p.rapidapi.com",
+      "x-rapidapi-key": "AIzaSyArySEMBpe3vPEMZcUfmiH7NMT_B9eaBao"
+    }
+  })
+  .then(response => response.json())
+  .then(data => {
+    if (data.results.length > 0) {
+      infoWindow.setContent(data.results[0].formatted_address);
+    } else {
+      infoWindow.setContent("Address Not Found");
+    }
+    infoWindow.setPosition(pos);
+    infoWindow.open(map);
+  })
+  .catch(error => {
+    console.error(error);
+    infoWindow.setContent("Error: Failed to fetch address");
+    infoWindow.setPosition(pos);
+    infoWindow.open(map);
+  });
+});
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(function(position) {
+    let pos = {
+      lat: position.coords.latitude,
+      lng: position.coords.longitude
+    };
+    infoWindow.open(map);
+    map.setCenter(pos);
+  }, function() {
+    handleLocationError(true, infoWindow, map.getCenter());
+  });
+} else {
+  handleLocationError(false, infoWindow, map.getCenter());
+}
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+infoWindow.setPosition(pos);
+infoWindow.setContent(browserHasGeolocation ?
+  'Error: The Geolocation service failed.' :
+  'Error: Your browser does not support geolocation.');
+infoWindow.open(map);
+}
