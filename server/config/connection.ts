34a3dadd300390ab2ad261ed172dcca813d7d641
anthonyero <anthonyero@gmail.com>
2024-06-25 19:41:17 -0500

    Defined a `connection.ts` file that will integrate with a MongoDB database - the models will be defined in a separate branch. In `server.ts`, defined a `startServer` function that incorporates our middleware and our database connection. Also, attached a `.env.example` file as a template

---
+++ server/config/connection.ts
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose');
+require('dotenv').config(); // Added to allow access to our local environmental variable that contains MONGODB_URI.
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/recommendaterDB');
+
+module.exports = mongoose.connection;
