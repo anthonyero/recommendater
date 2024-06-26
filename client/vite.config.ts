4a24667d2ea424aca7de3d696dbc201bc011e9c4
anthonyero <anthonyero@gmail.com>
2024-06-25 20:01:25 -0500

    Modified `vite.config.ts` within the 'client' directory to run the client's server on PORT 3000 and the API server on PORT 3001. This is useful information for other developers examining the code. On PORT 3000, the hot-module-reloading is functioning as expected. Updated the application's title to "RecommenDater" in `index.html` and the `<h1>` tag in `App.tsx`

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+    open: true,
+    fs: {
+      allow: [
+        // Allow serving files from the project root directory
+        path.resolve(__dirname),
+        // Allow serving files from the client directory
+        path.resolve(__dirname, 'client'),
+        // Allow serving files from the node_modules directory for slick-carousel fonts
+        path.resolve(__dirname, 'node_modules/slick-carousel/slick/fonts'),
+      ],
+    },
+  },
 })
