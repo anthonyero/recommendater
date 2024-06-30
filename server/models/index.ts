3d7d6d8f3bf72b6214fe28b3cd7431f76b7ee773
anthonyero <anthonyero@gmail.com>
2024-06-30 16:53:33 -0500

    Defined a 'Activity' schema and model in `Activity.ts`. Imported into `index.ts`

--- server/models/index.ts
+++ server/models/index.ts
@@ -1,5 +1,6 @@
 const User = require('./User.ts')
 const DateRecommendation = require('./DateRecommendation.ts');
 const Restaurant = require('./Restaurant.ts');
+const Activity = require('./Activity.ts');
 
 module.exports = { User, DateRecommendation }
