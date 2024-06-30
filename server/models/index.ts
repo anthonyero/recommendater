93fc2bce7864c2695ee9370f9ff929f419ee9f30
anthonyero <anthonyero@gmail.com>
2024-06-30 16:49:39 -0500

    Defined a schema and model for restaurant recommendations. This can be found within `Restaurant.ts`. Imported into `index.ts` in the 'models' sub-directory.

--- server/models/index.ts
+++ server/models/index.ts
@@ -1,4 +1,5 @@
 const User = require('./User.ts')
 const DateRecommendation = require('./DateRecommendation.ts');
+const Restaurant = require('./Restaurant.ts');
 
 module.exports = { User, DateRecommendation }
