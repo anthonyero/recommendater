596c97eb40e4e560cc6a6c1c72a6e022d7425c58
anthonyero <anthonyero@gmail.com>
2024-06-30 17:17:12 -0500

    Defined a 'Dessert' model and schema. Imported into `index.ts`

---
+++ server/models/Dessert.ts
@@ -0,0 +1,58 @@
+import { Schema, model } from 'mongoose';
+
+const dessertSchema = new Schema({
+	yelpId: {
+		type: String,
+		required: true
+	},
+	alias: {
+		type: String,
+		required: true
+	},
+	name: {
+		type: String,
+		required: true
+	},
+	imageUrl: {
+		type: String,
+		required: true,
+		match: [/(?:http[s]?:\/\/.)?(?:www\.)?[-a-zA-Z0-9@%._\+~#=]{2,256}\.[a-z]{2,6}\b(?:[-a-zA-Z0-9@:%_\+.~#?&\/\/=]*)/, 'Must match a URL']
+	},
+	yelpUrl: {
+		type: String,
+		required: true,
+		match: [/(?:http[s]?:\/\/.)?(?:www\.)?[-a-zA-Z0-9@%._\+~#=]{2,256}\.[a-z]{2,6}\b(?:[-a-zA-Z0-9@:%_\+.~#?&\/\/=]*)/, 'Must match a URL']
+	},
+	rating: {
+		type: Number,
+		required: true
+	},
+	reviewCount: {
+		type: Number,
+		required: true
+	},
+	price: {
+		type: String,
+		required: true
+	},
+	city: {
+		type: String,
+		required: true
+	},
+	country: {
+		type: String, 
+		required: true
+	},
+	state: {
+		type: String, 
+		required: true
+	},
+	displayAddress: {
+		type: String,
+		required: true
+	}
+});
+
+const Dessert = model('dessert', dessertSchema);
+
+module.exports = Dessert;
--- server/models/index.ts
+++ server/models/index.ts
@@ -2,5 +2,6 @@ const User = require('./User.ts')
 const DateRecommendation = require('./DateRecommendation.ts');
 const Restaurant = require('./Restaurant.ts');
 const Activity = require('./Activity.ts');
+const Dessert = require('./Dessert.ts');
 
-module.exports = { User, DateRecommendation }
+module.exports = { User, DateRecommendation, Restaurant, Activity, Dessert }
