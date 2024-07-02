ee3ab95d6c5c78d109d54ebe73d5e59a8c690110
anthonyero <anthonyero@gmail.com>
2024-07-01 19:22:35 -0500

    To begin seeding the database, I have created a `seed.ts` document in a newly created "seeds" sub-directory contained within "server". Currently, I have defined a set of 3 users in `userData.json` and wrote a script to insert these into our "users" collection in MongoDB. Also, at the root-level, installed `ts-node` as a dev dependency to run the `seed` script at that level

---
+++ server/seeds/seed.ts
@@ -0,0 +1,35 @@
+import { Types } from "mongoose";
+
+const db = require('../config/connection.ts');
+// const { User } = require( '../models/');
+// Import the example data
+const userData = require('./userData.json');
+
+interface user {
+	email: string,
+	password: string,
+	dates: Array<Types.ObjectId>,
+	_id: Types.ObjectId,
+	__v: number
+}
+
+// const cleanDB = require('./cleanDb.ts');
+
+db.once('open', async () => {
+	let { User } = require( '../models/'); // Declare these using let in functional scope gets past the error of const User being declared in `index.ts` from models
+
+	try {
+		// Clean the database collections
+
+		// Begin seeding the database
+		const users: Array<user> = await User.create(userData);
+		const userIds: Array<Types.ObjectId> = users.map(user => user._id)
+		console.log(userIds);
+
+		console.log('Seeding complete');
+    	process.exit(0);
+
+	} catch (err) {
+		throw err;
+	}
+})
