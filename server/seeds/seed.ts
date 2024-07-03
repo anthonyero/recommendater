f8eb6c28482ba9963a5ed64c445576931f181898
anthonyero <anthonyero@gmail.com>
2024-07-03 00:25:58 -0500

    Defined a few location data points for `dateRecommendationData.json`. All other values for this model depend on the Object Ids generated upon seeding so they cannot be predefined. In `seed.ts`: imported the data, defined a "dateRecommendation" interface, and was able to successfully seed the "daterecommendations" collection with these newly generated documents. Initially, I had issues resolving the promise when iterating over an array of Promises but the following [link](https://stackoverflow.com/questions/40140149/use-async-await-with-array-map) was useful in resolving the issue. The rationale is also detailed in comments.
    The final step necessary is updating the "User" documents and adding their respective "dateRecommendations" to their document. This will be submitted in a future commit

--- server/seeds/seed.ts
+++ server/seeds/seed.ts
@@ -1,12 +1,7 @@
 import { Types } from "mongoose";
 const db = require('../config/connection.ts');
 
-// Import the example data
-const userData = require('./userData.json');
-const restaurantData = require('./restaurantData.json');
-const activityData = require('./activityData.json');
-const dessertData = require('./dessertData.json');
-
+// Define interfaces for each of our models
 interface user {
 	email: string,
 	password: string,
@@ -34,10 +29,30 @@ interface business {
 	__v: number
 }
 
-// const cleanDB = require('./cleanDb.ts');
+interface dateRecommendation {
+	user: Types.ObjectId,
+	date: Date,
+	location: string,
+	latitude: number,
+	longitude: number,
+	restaurant: Types.ObjectId,
+	activity: Types.ObjectId,
+	dessert: Types.ObjectId,
+	favorite: Boolean,
+	_id: Types.ObjectId,
+	__v: number
+}
 
+// Import the example data
+const userData: Array<user> = require('./userData.json');
+const restaurantData: Array<business> = require('./restaurantData.json');
+const activityData: Array<business> = require('./activityData.json');
+const dessertData: Array<business> = require('./dessertData.json');
+const dateRecommendationData: Array<dateRecommendation> = require('./dateRecommendation.json')
+
+// Initiate connection to databse
 db.once('open', async () => {
-	let { User, Restaurant, Activity, Dessert } = require( '../models/'); // Declare these using let in functional scope gets past the error of const User being declared in `index.ts` from models
+	let { User, Restaurant, Activity, Dessert, DateRecommendation } = require( '../models/'); // Declare these using let in functional scope gets past the error of const User being declared in `index.ts` from models
 	let cleanDB = require('./cleanDb.ts');
 
 	try {
@@ -46,6 +61,7 @@ db.once('open', async () => {
 		await cleanDB('Restaurant', 'restaurants');
 		await cleanDB('Activity', 'activities');
 		await cleanDB('Dessert', 'desserts');
+		await cleanDB('DateRecommendation', 'daterecommendations');
 
 		// Begin seeding the database
 		const users: Array<user> = await User.create(userData);
@@ -63,6 +79,23 @@ db.once('open', async () => {
 		const desserts: Array<business> = await Dessert.create(dessertData);
 		const dessertIds: Array<Types.ObjectId> = desserts.map(dessert => dessert._id);
 		console.log(dessertIds);
+
+		// Promise.all with await required otherwise we have undefined because the Promise is unfulfilled/not resolved. 
+		// We want to return an array of Promises so we first need to contain these operations within a single Problem and await its resolution
+		const dateRecommendations: Array<dateRecommendation> = await Promise.all(dateRecommendationData.map(async (dateRecommendation: dateRecommendation, index: number) => {
+            const recommendation: dateRecommendation = await DateRecommendation.create({
+                user: userIds[index],
+                restaurant: restaurantIds[index],
+                activity: activityIds[index],
+                dessert: dessertIds[index],
+                location: dateRecommendationData[index].location,
+                latitude: dateRecommendationData[index].latitude,
+                longitude: dateRecommendationData[index].longitude
+            });
+            return recommendation;
+        }));
+		const dateRecommendationIds: Array<Types.ObjectId> = dateRecommendations.map( dateRecommendation => dateRecommendation._id);
+		console.log(dateRecommendationIds);
 
 		console.log('Seeding complete');
     	process.exit(0);
