8694718afa89c9bc94541638b744695212bec710
anthonyero <anthonyero@gmail.com>
2024-08-09 00:26:36 -0500

    From the 'server/routes/index.ts` file, I removed the wildcard route giving preference to the wildcard used for "production" deployment where it will redirect to our React application.
    When testing for development, requests to incorrect/undefined endpoints will simply result in a 404 error. In production, the React application will be rendered.

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,8 +1,8 @@
-import { Request, Response } from 'express';
+// import { Request, Response } from 'express';
 const router = require('express').Router();
 const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
-router.use((req: Request, res: Response) => res.send('Wrong route!'));
+// router.use((req: Request, res: Response) => res.send('Wrong route!'));
 
 module.exports = router;
