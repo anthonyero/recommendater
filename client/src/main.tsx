2cad16409f2e03463696ba343db089187e3b8c1c
anthonyero <anthonyero@gmail.com>
2024-08-13 20:55:14 -0500

    Following along with Auth0 documentation to implement with a Single Page Application and React:
    **Install the Auth0 React SDK**
    
    - In the 'client' directory, I added `@auth0/auth0-react` to `package.json`
    - In `client/src/main.tsx`:
        - I imported `Auth0Provider`
        - Wrapped the entire application in the `Auth0Provider` component
        - According to the documentation and other forums, it seems that the `domain` and `clientId` props do not have to be secret
    
    `Auth0Provider` stores the authentication state of the users and the state of the SDK. Auth0 React SDK uses React Context to manage the authentication state of users.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain='dev-q6ysbscflfuvzzf8.us.auth0.com'
+      clientId='2RNUWugrFHPGkNdCMI0kw2jIqojr5H5S'
+      authorizationParams={{
+        redirect_uri: window.location.origin
+      }}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>,
 )
