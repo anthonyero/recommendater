df8ad3bf96504de0f5b125e75a7910d0e35880bc
anthonyero <anthonyero@gmail.com>
2024-08-14 21:20:23 -0500

    In `App.tsx`:
    - Imported `outlet` from `react-router-dom`
    - Imported `Auth0Provider`
    - Imported 'Navbar'
    
    Passed the `Auth0Provider` as a wrapper to encompass the entire app. Imported and passed the 'Navbar' component, and below it rendered the 'Outlet' component which will connect to our router and render the selected element/component.
    
    A handful of the tests and state code was removed from `App.tsx` to `Home.tsx`

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,52 +1,27 @@
-import { useState } from 'react'
-import LoginButton from './components/LoginButton';
-import LogoutButton from './components/LogoutButton';
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import { Auth0Provider } from '@auth0/auth0-react';
+import Navbar from './components/Navbar';
 import './App.css'
 
 function App() {
-  interface textObject {
-    text: string
-  }
-  const [count, setCount] = useState(0)
-  const [testData, setTestData] = useState('')
-
-  const testRequest = async (url: string): Promise<any> => {
-    const options: object = {
-      method: 'GET'
-    }
-    try {
-      const response = await fetch(url, options) // Returns a response object with type, status, etc
-      const data: textObject = await response.json() // Must use await otherwise we receive a undefined
-      setTestData(data.text)
-      return data
-
-    } catch (err) {
-        if (err) {
-          return err
-        }
-    }
-  }
-
+  
   return (
-    <>
-      <h1>RecommenDater</h1>
-      <LoginButton></LoginButton>
-      <LogoutButton></LogoutButton>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <button onClick={() => testRequest('api/text')}>API test</button>
-      <p>{testData}</p>
-    </>
+    <React.StrictMode>
+      <Auth0Provider
+        domain='dev-q6ysbscflfuvzzf8.us.auth0.com'
+        clientId='2RNUWugrFHPGkNdCMI0kw2jIqojr5H5S'
+        authorizationParams={{
+          redirect_uri: window.location.origin
+        }}
+      >
+        <h1>RecommenDater</h1>
+        <Navbar />
+        <Outlet />
+    </Auth0Provider>
+  </React.StrictMode>
+    
   )
-}
+};
 
-export default App
+export default App;
