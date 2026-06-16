e9ba8d6805ca6abbdec58fdaaa428640335b67bd
anthonyero <anthonyero@gmail.com>
2026-06-15 21:22:23 -0500

    Created a  router and imported it into our server  router

--- server/routes/api/index.ts
+++ server/routes/api/index.ts
@@ -1,6 +1,8 @@
 const apiRouter = require('express').Router();
 const yelpRoutes = require('./yelpRoutes'); // Removed the hardcoded .ts extension which was creating errors when built and using npm run start 
+const userRoutes = require('./userRoutes.ts');
 
 apiRouter.use('/yelp', yelpRoutes);
+apiRouter.use('/user', userRoutes)
 
 module.exports = apiRouter;
---
+++ server/routes/api/userRoutes.ts
@@ -0,0 +1,7 @@
+const userRouter = require('express').Router();
+
+// Endpoint is '/api/user/search'
+userRouter.route('login')
+	.get()
+
+module.exports = userRouter;
